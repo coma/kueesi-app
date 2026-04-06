f1dee967bfe433682229c85202a00be1d2da7aff
Eduardo García Sanz <eduardo@comakai.com>
2026-04-06 13:29:51 +0200

    useRefreshTokens

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -6,6 +6,7 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
+      useRefreshTokens
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
