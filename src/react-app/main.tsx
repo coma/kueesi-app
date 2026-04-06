b96c0fcdb2fd5c6a375a91c097572c8f5e194852
Eduardo García Sanz <eduardo@comakai.com>
2026-04-06 13:45:42 +0200

    cacheLocation

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -7,6 +7,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
       useRefreshTokens
+      cacheLocation="localstorage"
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
