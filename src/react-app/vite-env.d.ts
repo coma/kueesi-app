cae2d97421867b41333a1d266398687bd282e8bd
Eduardo García Sanz <eduardo@comakai.com>
2026-04-18 20:50:14 +0200

    address type issues

--- src/react-app/vite-env.d.ts
+++ src/react-app/vite-env.d.ts
@@ -1,4 +1,13 @@
 /// <reference types="vite/client" />
+import "react";
+
+declare module "react" {
+  interface ButtonHTMLAttributes<T> {
+    command?: string;
+    commandfor?: string;
+  }
+}
+
 interface ImportMetaEnv {
   readonly VITE_AUTH0_DOMAIN: string;
   readonly VITE_AUTH0_CLIENT_ID: string;
